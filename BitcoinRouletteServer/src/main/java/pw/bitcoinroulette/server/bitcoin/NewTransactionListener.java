61e737cd08a9af9c042718cec77c7e1db1ae3b92
Brian Bergeron <bergeron@cmu.edu>
2015-03-06 02:29:17 -0500

    fix race condition for incoming transactions within same wallet

--- BitcoinRouletteServer/src/main/java/pw/bitcoinroulette/server/bitcoin/NewTransactionListener.java
+++ BitcoinRouletteServer/src/main/java/pw/bitcoinroulette/server/bitcoin/NewTransactionListener.java
@@ -3,10 +3,14 @@ package main.java.pw.bitcoinroulette.server.bitcoin;
 import java.util.Observable;
 import java.util.Observer;
 
-import main.java.pw.bitcoinroulette.server.AuthPlayerImpl;
+import main.java.pw.bitcoinroulette.server.models.AuthPlayerImpl;
+import main.java.pw.bitcoinroulette.server.models.TransactionImpl;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Projection;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
 
 import com._37coins.bcJsonRpc.pojo.Transaction;
 
@@ -23,34 +27,53 @@ public class NewTransactionListener implements Observer {
 	 * Called twice for each incoming or outgoing transaction.
 	 * Once when first seen by network (0 confirmations),
 	 * Again when in first block (1 confirmation)
+	 * (Intra-wallet transactions will get double called, once for send, once for receive)
 	 */
 	@Override
-	public void update(Observable o, Object arg) {
+	public synchronized void update(Observable o, Object arg) {
 		Transaction mainTx = (Transaction) arg;
-		String txid = mainTx.getTxid();
+		String txHash = mainTx.getTxid();
 		long conf = mainTx.getConfirmations();
 
 		/* Only care about new transactions */
 		if (conf != 0) {
 			return;
 		}
-		
+
 		Session session = sessionFactory.openSession();
 
 		for (Transaction t : mainTx.getDetails()) {
+
 			/* Only care about incoming transactions */
 			if (t.getCategory() == Transaction.Category.RECEIVE) {
-				System.out.println(txid);
-				
+				System.out.printf("new incoming tx: %s\n", txHash);
+
 				session.beginTransaction();
+
+				/* Should only be seen if sent & received from within same wallet */
+				boolean seen = (long)(session.createCriteria(TransactionImpl.class)
+						.add(Restrictions.eq("hash", txHash))
+						.setProjection(Projections.rowCount())
+						.uniqueResult()) == 1;
+
+				if(seen){
+					continue;
+				}
+
 				AuthPlayerImpl player = (AuthPlayerImpl) session
 						.createQuery("from AuthPlayerImpl where bitcoinAddress = :bitcoinAddress")
 						.setString("bitcoinAddress", t.getAddress()).uniqueResult();
+
+				if (player == null) {
+					System.err.printf("No player associated with address: %s\n", t.getAddress());
+				} else {
+					session.save(new TransactionImpl(txHash, t.getAmount(), conf, mainTx.getTime(), player));
+				}
+
 				session.getTransaction().commit();
-				System.out.println(player);
 			}
 		}
-		
+
 		session.close();
 	}
 }
